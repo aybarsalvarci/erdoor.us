bb0beaf64ea76a332b433f2c4127ee5476eae57c
aybarsalvarci <aybarsalvarci44@gmail.com>
2026-07-26 21:17:33 +0300

    Door spesification silme işlemleri.

--- app/Http/Controllers/Admin/DoorSpesificationController.php
+++ app/Http/Controllers/Admin/DoorSpesificationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\DoorSpesification\CreateDoorSpesificationRequest;
 use App\Models\Door;
+use App\Models\DoorSpesification;
 use Illuminate\Support\Facades\Log;
 
 class DoorSpesificationController extends Controller
@@ -36,6 +37,20 @@ class DoorSpesificationController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $spec = DoorSpesification::findOrFail($id);
+
+        try {
+            $spec->delete();
+            return redirect()->back()->with([
+                'success' => "Door spesification deleted successfully",
+                'tab' => 'specs'
+            ]);
+        } catch (\Exception $exception) {
+            Log::error("Door spesification deletion failed: " . $exception->getMessage(), ['exception' => $exception]);
+            return redirect()->back()->with([
+                'error' => "An error occured while deleting Door Spesification",
+                'tab' => 'specs'
+            ]);
+        }
     }
 }
